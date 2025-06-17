9bb6c4739d9e500315f9444c7c7cc6628ab363ef
CharlesSinchigalo <147677534+CharlesSinchigalo@users.noreply.github.com>
2025-06-16 22:15:54 -0500

    Agrega CORS para permitir acceso desde Flutter web

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from models import Base
 from database import engine
 from routers import usuarios, auditoria
 
 app = FastAPI()
 
-# Crear las tablas automáticamente si no existen
+# Configuración CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes poner aquí el dominio exacto si lo deseas restringir
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Crear tablas si no existen
 Base.metadata.create_all(bind=engine)
 
 # Incluir rutas
